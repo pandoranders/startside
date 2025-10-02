ec0d58a4ec407332924e7a9a5d9a48b5ac60b823
Anders Sletbak Nilsen <anders@dyrehagen.org>
2025-10-02 11:54:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function showEasterEgg() {
     const sparkle = document.getElementById('sparkle');
     
     if (egg.innerText.includes('🐻')) {
-    egg.innerHTML = '🐻<span id="sparkle" class="sparkle">✨</span>';
+    egg.innerHTML = '🦄<span id="sparkle" class="sparkle">✨</span>';
     } else {
     egg.innerHTML = '🐻<span id="sparkle" class="sparkle">✨</span>';
     }
